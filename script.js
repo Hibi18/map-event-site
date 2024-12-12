a8f1961b09e8668880e2b181bc0dcbcdfcd472ba
Hibi18 <hibiki160108@icloud.com>
2024-12-12 23:14:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+// header.html を読み込んで挿入
 fetch('header.html')
   .then(response => response.text())
   .then(data => {
     document.getElementById('header-container').innerHTML = data;
-  });
+  })
+  .catch(error => console.error('Error loading header:', error));
